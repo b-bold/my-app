b7e649aaefd4d813ad577c833705820036759310
b-bold <anneikanicole@gmail.com>
2023-06-13 16:47:00 -0400

    notes to add edit and delete abilities

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,13 +7,21 @@ const App = () => {
     const [todos, setTodos] = useState<Array<Todo>>([]);
     const [todoValue, setTodoValue] = useState("");
 
+    // think of errors!! We aren't handling any. There should be restrictions to keep from adding only 
+    // a letter, only numbers or symbols. I think my old team used Yup for this. i'll Look into it #lookup
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        // change logic here to also check if the todo id already exists. 
+        // if it doesn't 
+            // set it as seen below
         setTodoValue(event.currentTarget.value);
+        // if it does exist, change the task value to the new value in the todos_hash
+        // then set the value in the UI. 
       };
 
       const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+        event.preventDefault(); // what does this do exactly? i think i can assume but need to #lookup
       
+        // save to todos_hash
         setTodos((previousTodos) => [
           ...previousTodos,
           {
